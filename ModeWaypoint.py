64f041d2ed034329374a2fb65b8babd36ba304c2
Bob-Z <carabobz@gmail.com>
2021-01-10 17:02:48 +0100

    Improve waypoint mode direction

--- ModeWaypoint.py
+++ ModeWaypoint.py
@@ -1,5 +1,3 @@
-import time
-
 import numpy as np
 
 import Command
@@ -9,14 +7,14 @@ import Traction
 
 # waypoint = [[512.0, 0.0, 512.0], [412.0, 0.0, 512.0]]
 
-waypoint = [[882.132996, 0.120613, 1324.317139, 150],
+waypoint = [[882.132996, 0.120613, 1324.317139, 70],
             [671.969, 0.628959, 1327.45, 70],
             [630.046, 0.495165, 1316.58, None],
             [600.246, 0.493378, 1287.67, None],
             [575.756, 0.495862, 1230.29, None],
             [576.293, 0.49587, 1135.25, None],
             [573.385, 0.492764, 1068.6, None],
-            [566.941, 0.498048, 1040.65, None],
+            [566.941, 0.498048, 1040.65, 70],
             [547.127, 0.495708, 1008.84, None],
             [522.018, 0.491687, 991.464, None],
             [450.689, 0.495061, 949.769, None],
@@ -91,17 +89,22 @@ def run():
         diff_rot = rotation[1] - target_angle
         # print('rotation[1] - target_angle:', diff_rot)
 
+        if diff_rot > 180.0:
+            diff_rot = diff_rot - 360.0
+        if diff_rot < -180.0:
+            diff_rot = 360.0 + diff_rot
         print("diff_rot = ", diff_rot)
+
+        #wheel_force = max(5, abs(diff_rot) * 1.5)
+        wheel_force = 10 + abs(diff_rot)
+        print("wheel force: ", wheel_force)
+
         if diff_rot > 0.0:
-            if diff_rot < 180.0:
-                Command.start_left(max(abs(diff_rot), 15))
-            else:
-                Command.start_right(max(abs(diff_rot), 15))
+            print("left")
+            Command.start_left(wheel_force)
         else:
-            if diff_rot < -180.0:
-                Command.start_left(max(abs(diff_rot), 15))
-            else:
-                Command.start_right(max(abs(diff_rot), 15))
+            print("right")
+            Command.start_right(wheel_force)
 
 
 ''' 
@@ -124,7 +127,7 @@ def check_waypoint_distance(current_waypoint, position):
     dist_x = position[0] - waypoint[current_waypoint][0]
     dist_y = position[2] - waypoint[current_waypoint][2]
     distance = (dist_x * dist_x + dist_y * dist_y)
-    #print("distance: ", distance)
+    # print("distance: ", distance)
     # meter square
     if distance < 100.0:
         new_waypoint = (current_waypoint + 1) % len(waypoint)
