657864539625bcb98744f7532b3100f7c8864bec
Bob-Z <carabobz@gmail.com>
2021-02-07 17:44:47 +0100

    Calculate rotation speed on RoR's input

--- Input.py
+++ Input.py
@@ -10,6 +10,7 @@ started = False
 position = None
 rotation = None
 speed = None
+rotation_speed = None
 lock = threading.Lock()
 
 
@@ -22,6 +23,7 @@ def init():
 
 def read_stdin(ror_input):
     previous_position = [0.0, 0.0, 0.0]
+    previous_rotation = [0.0, 0.0, 0.0]
     previous_timestamp = datetime.datetime.now()
     while True:
         try:
@@ -35,19 +37,23 @@ def read_stdin(ror_input):
                     global position
                     global rotation
                     global speed
+                    global rotation_speed
                     lock.acquire()
                     position = [float(numeric_value[0]), float(numeric_value[1]), float(numeric_value[2])]
                     rotation = [float(numeric_value[3]), float(numeric_value[4]), float(numeric_value[5])]
                     global started
                     if started is False:
                         previous_position = position
+                        previous_rotation = rotation
                         speed = [0.0, 0.0, 0.0]
+                        rotation_speed = [0.0, 0.0, 0.0]
                         started = True
                     else:
                         duration = timestamp - previous_timestamp
                         # print("duration", duration.total_seconds())
                         for i in range(3):
                             speed[i] = (position[i] - previous_position[i]) / duration.total_seconds()
+                            rotation_speed[i] = (rotation[i] - previous_rotation[i]) / duration.total_seconds()
 
                         previous_timestamp = timestamp
                         previous_position = position
@@ -98,6 +104,16 @@ def get_speed():
 def get_norm_speed():
     global speed
     return math.sqrt(speed[0] * speed[0] + speed[1] * speed[1] + speed[2] * speed[2])
+
+
+def get_rotation_speed():
+    global lock
+    global rotation_speed
+    lock.acquire()
+    ret_speed = rotation_speed
+    lock.release()
+
+    return ret_speed
 
 
 def is_started():
