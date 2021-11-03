2cd7c38538684b1f35e5830fda473e7e5eb35336
Bob-Z <carabobz@gmail.com>
2021-11-03 22:19:50 +0100

    Add point_in_rectangle function

--- Math.py
+++ Math.py
@@ -13,3 +13,26 @@ def calc_angle(p0, p1, p2):
 
     angle = numpy.math.atan2(numpy.linalg.det([v0, v1]), numpy.dot(v0, v1))
     return numpy.degrees(angle)
+
+
+# http://stackoverflow.com/a/37865332/276093
+
+def point_in_rectangle(point, rectangle):
+    AB = vector(rectangle[0], rectangle[1])
+    AP = vector(rectangle[0], point)
+    BC = vector(rectangle[1], rectangle[2])
+    BP = vector(rectangle[1], point)
+    dotABAP = dot(AB, AP)
+    dotABAB = dot(AB, AB)
+    dotBCBP = dot(BC, BP)
+    dotBCBC = dot(BC, BC)
+
+    return 0 <= dotABAP <= dotABAB and 0 <= dotBCBP <= dotBCBC
+
+
+def vector(p1, p2):
+    return [(p2[0] - p1[0]), (p2[1] - p1[1])]
+
+
+def dot(u, v):
+    return u[0] * v[0] + u[1] * v[1]
